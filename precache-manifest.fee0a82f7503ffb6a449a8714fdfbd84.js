c485c76197a030529be3db38a4298c702650ca96
Code Sandbox <hello@codesandbox.io>
2023-03-04 02:05:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fee0a82f7503ffb6a449a8714fdfbd84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a4a7827bf1c7ce3c78",
+    "url": "https://bittssss.github.io/csb-g5esit/static/css/main.98d9e5cb.chunk.css"
+  },
+  {
+    "revision": "57a4a7827bf1c7ce3c78",
+    "url": "https://bittssss.github.io/csb-g5esit/static/js/main.57a4a782.chunk.js"
+  },
+  {
+    "revision": "919d3ca51a4124cc6aa7",
+    "url": "https://bittssss.github.io/csb-g5esit/static/js/1.919d3ca5.chunk.js"
+  },
+  {
+    "revision": "a831fd65bef0366f0151",
+    "url": "https://bittssss.github.io/csb-g5esit/static/js/runtime~main.a831fd65.js"
+  },
+  {
+    "revision": "f350f50466a5a884bdc9c78b6d19d094",
+    "url": "https://bittssss.github.io/csb-g5esit/index.html"
+  }
+];
